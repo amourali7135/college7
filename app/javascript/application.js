1ce5274cf579f537c1768d1ba82d772e4f79925b
Amir Mourali <amourali7135@gmail.com>
2022-06-20 18:32:28 +0800

    JS functions imported same way now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,17 +6,17 @@ import "./controllers"
 import "bootstrap"
 
 import { initProgramform, _initProgramform }
-from '../functions/init_programform';
-import { initUserform, _initUserform } from '../functions/init_userform';
-import { initAutocomplete, _initAutocomplete } from '../functions/init_autocomplete';
+from './init_programform.js';
+import { initUserform, _initUserform } from './init_userform.js';
+import { initAutocomplete, _initAutocomplete } from './init_autocomplete.js';
 
 // my own experiment 
 document.addEventListener("DOMContentLoaded", () => {
     // Your code to run since DOM is loaded and ready
-    initProgramform();
-    initUserform();
-    initAutocomplete();
+    
   });
   
   
-  
+  initProgramform();
+    initUserform();
+    initAutocomplete();
